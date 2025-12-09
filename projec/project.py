bcab0651667f2a90ce99ba9b6a53ce9afd12ece4
chaymaatlm735-arch <chaymaatlm735@gmail.com>
2025-12-09 20:41:34 +0000

    first commit

---
+++ projec/project.py
@@ -0,0 +1,63 @@
+#Boussaid chaimaa M1 immunologie 08/12/2025
+#Belkenadil bouchra sanaa nesrine
+#Kaid ines safia
+#Djabeur djezzar djezia 
+#Benameur hidayat
+#Hamzaoui aroua 
+
+
+
+import pandas as tb
+# donnes : sequences ADN , Longeur , Pourcentage GC
+data ={ 
+    "sequence":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT"," TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+    "Longeur" :[12,12,12,10,11,10,10],
+    "Pourcentage GC":[50,66.67,58.33,40,45.45,60,50]   
+
+}
+#creation d un DataFrame (tableau pandas)
+df= tb.DataFrame(data)
+print("************** Creation et Affichage ***************","\n""\n")
+#affichage de tableau 
+print("Tableau des sequences ADN :", "\n")
+print(df)
+# afficher uniquement la Longeur 
+Longeur =df["Longeur"]
+print("********* longeur *********","\n")
+print(Longeur,"\n")
+# filtrer la longeur > 10 
+print("********* Filtrage avec longeur >10 ********* ")
+filtered_df = df[df["Longeur"]>10]
+print(filtered_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#sauvgarder et chargement des donnes avec panda 
+df.to_csv("tableau_sequences.csv",inedx=False)
+
+#telecharger un fichier csv et mettre dans dataframe
+df_loaded = tb.read_csv("tableau_sequences.csv")
+print(df_loaded)
