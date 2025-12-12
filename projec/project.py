a241c8d2326df0aece49c114cc826aeea6f36926
Arouahamz-immuno1 <hzarwa999@gmail.com>
2025-12-12 19:29:12 +0100

    Update project.py
    Résolution de la question 7 et amélioration du projet

--- projec/project.py
+++ projec/project.py
@@ -1,85 +1,83 @@
-#Boussaid chaimaa M1 immunologie 08/12/2025
-#Belkenadil bouchra sanaa nesrine
-#Kaid ines safia
-#Djabeur djezzar djezia 
-#Benameur hidayat
-#Hamzaoui aroua 
-
+#Boussaid Chaimaa M1 immunologie 08/12/2025
+#Belkenadil Bouchra Sanaa Nesrine
+#Kaid Ines Safia
+#Djabeur Djezzar Djezia 
+#Benameur Hidayat
+#Hamzaoui Aroua 
 
 
 import pandas as tb
 import numpy as np
-# donnes : sequences ADN , Longeur , Pourcentage GC
+# Données : séquences ADN , Longueur , Pourcentage GC
 data ={ 
-    "sequence":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT"," TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
-    "Longeur" :[12,12,12,10,11,10,10],
+    "Séquence":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT"," TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+    "Longueur" :[12,12,12,10,11,10,10],
     "Pourcentage GC":[50,66.67,58.33,40,45.45,60,50]   
-
 }
-#creation d un DataFrame (tableau pandas)
+
+# 1)Créer et afficher le tableau en utilisant la bibliothèque pandas:
+# Création de tableau :
 df= tb.DataFrame(data)
-print("************** Creation et Affichage ***************","\n""\n")
-#1)affichage de tableau 
-print("Tableau des sequences ADN :", "\n")
-print(df)
-# 2)afficher uniquement la Longeur 
-Longeur =df["Longeur"]
-print("********* longeur *********","\n")
-print(Longeur,"\n")
-#3) filtrer la longeur > 10 
-print("********* Filtrage avec longeur >10 ********* ")
-filtered_df = df[df["Longeur"]>10]
-print(filtered_df)
+print("1 ********** Création et Affichage du tableau **********","\n""\n")
 
-#4Calculer la moyenne du pourcentage de GC
-print("************* Calcul de la moyenne *************")
+#Affichage de tableau :
+print(df,"\n""\n")
+# 2)a-Sélectionner  uniquement la colonne “Longueur”:
+Longueur =df["Longueur"]
+# 2)Afficher uniquement la colonne "Longueur":
+print("2 ********** Affichage de la colonne longueur **********","\n""\n")
+print(Longueur,"\n""\n")
+
+# 3) Filtrer les séquences dont la longueur est > 10 :
+print("3 ********** Filtrage dont la longueur est > 10 **********","\n""\n")
+filtered_df = df[df["Longueur"] > 10]
+print(filtered_df,"\n""\n")
+
+
+#4) Calculer le pourcentage moyen de GC:
+print("4 ********** Calcul de pourcentage moyen de GC **********","\n""\n")
+
 average_gc = df["Pourcentage GC"].mean()
-print(f"Pourcentage moyen de GC : {average_gc:.3f}%")
+print(f"Le Pourcentage moyen de GC est: {average_gc:.3f}%")
+print("\n")
 
-#5) Ajouter une colonne "Catégorie GC"
+
+# 5) Ajouter une colonne "Catégorie GC":
+print("5 ********** Ajout d'une colonne 'Catégorie GC' **********","\n""\n")
 conditions = [
     (df['Pourcentage GC'] >= 55),
     (df['Pourcentage GC'] >= 45) & (df['Pourcentage GC'] < 55),
     (df['Pourcentage GC'] < 45)
 ]
-
 choix = ['Riche', 'Moyen', 'Faible']
 df['categorie GC'] = np.select(conditions, choix, default='inconnu')
-
-print(df)
-
- #6) Ajouter une colonne donnant le nombre de "G" dans chaque séquence
-df['Nombre de G'] = df["sequence"].str.count("G")
-
-print("===== nombre de G ajoutés ======")
-print(df, "\n")
+print(df,"\n""\n")
 
 
+ # 6) Ajouter une colonne donnant le nombre de "G" dans chaque séquence:
+print("6 ********** Ajout d'une colonne donnant le nombre de G **********","\n""\n")
+df["Nombre de G"] = df["Séquence"].str.count("G")
+print(df,"\n")
 
 
+# 7) Calculer l‟écart-type du %GC et de la longueur des séquences:
+print("7 ********* Calcul de l'écart-type du %GC & de la longueur *********","\n")
+data = {
+    "%GC": [50, 66.67, 58.33, 40, 45.45, 60, 50],
+    "Longueur": [12, 12, 12, 10, 11, 10, 10]
+}
+écart_type_GC = df["Pourcentage GC"].std()
+écart_type_long = df["Longueur"].std()
+
+print("Écart-type du %GC est:", round(écart_type_GC,3))
+print("Écart-type de la longueur des séquences est :", round(écart_type_long))
+#(On écrit :.1f si l’on veut garder un seul chiffre après la virgule)
+#(On écrit :.2f si l’on veut garder deux chiffre après la virgule)...
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#8)sauvgarder et chargement des donnes avec panda 
+# 8) Sauvgarde et chargement des données avec panda:
 df.to_csv("tableau_sequences.csv",inedx=False)
 
-#8)telecharger un fichier csv et mettre dans dataframe
+#8) Télécharger un fichier csv et l'importer dans un Dataframe:
 df_loaded = tb.read_csv("tableau_sequences.csv")
 print(df_loaded)
